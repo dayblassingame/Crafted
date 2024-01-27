125bdbe74235fc1a688b85857634f98b26c4de5b
Day Blassingame <dayblassingame@gmail.com>
2024-01-27 12:12:46 -0500

    open nav functionality

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { useState } from "react";
 import Logo from './images/craftedlogo.png';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header(){
+    const [menu, setMenu] = useState(false);
 
     return(
         <header>
              <span className="CC-C-logo_span ">
                 <a href="#"><img src={Logo} alt="Logo" className="CC-C-logo_img"/></a>
             </span>
-            <button><FontAwesomeIcon icon={faBars} /></button>
-            <nav>
+            <button className={menu ? 'display_none': 'display_inline-block'} onClick={()=>{setMenu(!menu);}}>
+                <FontAwesomeIcon icon={faBars} />
+            </button>
+            <nav className={menu ? 'display_block': 'display_none'}>
                 <span>
                     <a href='#'>Discover New</a>
                 </span>
