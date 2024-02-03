7512c10afe5f64ccbf9f33e2f641572de80e80ae
Day Blassingame <dayblassingame@gmail.com>
2024-02-03 02:16:55 -0500

    footer frame

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import './App.scss';
+import Footer from './Footer';
 
 export function App(){
     return(
         <div>
            <Header/>
+           <Footer/>
         </div>
     )
 }
