28a333249f4094b74d94fc9f97ff0cd15ed8126e
Day Blassingame <dayblassingame@gmail.com>
2024-02-02 19:59:14 -0500

    animate hero text on load

--- src/Api.js
+++ src/Api.js
@@ -1,32 +1,18 @@
 import React, { useCallback, useState } from "react";
 import axios from "axios";
 
-export default function fetchCocktailData(){
-    const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=greyhound';
-    const [data, setData] = useState([]);
+export async function fetchHeroData(){
+    let data = [];
+    const displayURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=greyhound';
 
-    const getData = useCallback(() => {
-        axios  
-            .get(url)
-            .then((response) =>{
-                console.log(response.data);
-                setData(response.data.drinks);
-            }).catch((e)=>{console.log(e)})
-    },[data])
+    axios  
+        .get(displayURL)
+        .then((response) =>{
+            data = (response.data.drinks);
+        }).catch((e)=>{console.log(e)})
+    
+    return {
+        props: data
+    }
 
-    return data;
 }
-
-// = useCallback(()=>{
-//     const [data, setData] = useState([]);
-
-//     const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=greyhound';
-
-//     axios.get(url).than(response => {
-//         console.log(response.data);
-//         setData(response.data.drinks);
-//     }).catch(e=>console.log(e))
-
-//     return data;
-
-//},[])
