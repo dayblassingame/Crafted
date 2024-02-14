81c4156c312a5e4fbdf4513b3d2cc58445c0b897
Day Blassingame <dayblassingame@gmail.com>
2024-02-13 23:47:48 -0500

    updated footer links

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer(){
+
+    const handleScroll = () =>{
+        window.scrollTo(0,0);
+    }
 
     return(
         <div className="CC-C-footer_wrapper">
             <h4>Explore Crafted</h4>
             <span className="CC-C-footer_navLinks">
-                <a>Cocktail Generator</a>
-                <a>Search Cocktails</a>
-                <a>Meet the author</a>
-                <a>Contact</a>
+                <li>
+                    <Link to='/random' onClick={handleScroll}>Random Cocktail Generator</Link>
+                </li>
+                <li>
+                    <Link to='/search' onClick={handleScroll}>Search Cocktails</Link>
+                </li>
+                <li>
+                    <a href='#' onClick={handleScroll}>Meet the author</a>
+                </li>
+                <li>
+                    <a href='#' onClick={handleScroll}>Contact</a>
+                </li>
             </span>
             <h4>Legal</h4>
             <span className="CC-C-footer_legal">
